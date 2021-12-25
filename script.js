97337d26611a1b097dcd471bf31f05b34b8a1f11
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-25 21:19:17 +0100

    Add an event listener to the 'Check' button

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// Add an event listener to the "Check!" button
+const checkButton = document.querySelector('.check');
+const input = document.querySelector('.guess');
+checkButton.addEventListener('click', (e) => {
+  const userGuess = input.value;
+  console.log(userGuess);
+})
