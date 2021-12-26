f5063be162e4136f6fb095d0d58d1abbba8471dc
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-26 10:01:04 +0100

    Refactor script.js

--- script.js
+++ script.js
@@ -16,21 +16,36 @@ const scoreSpan = document.querySelector('.score');
 
 let score = 20;
 
+// CSS changes if a correct answer is given
+const changeStyleOnCorrect = () => {
+  const number = document.querySelector('.number');
+  number.innerHTML = secretNumber;
+  number.style.background = '#678D58';
+  number.style.color = '#FFF8F0';
+  number.style.fontWeight = '700';
+  message.style.color = '#678D58';
+  message.style.fontWeight = '700';
+}
+
+// Update score if the answer is wrong and display 'too high!' or 'too low!'
+const updateScore = (msg) => {
+  score--;
+  scoreSpan.innerHTML = score;
+  if (score > 0) {
+    message.innerHTML = msg;
+  } else {
+    message.innerHTML = "Game over!"
+  }
+}
+
 // Check user's input
 checkButton.addEventListener('click', (e) => {
   const userGuess = Number(input.value);
-  // if the user provided no number - display a message 'No number!'
   if (!userGuess) {
     message.innerHTML = "No number!"
-    // if the user's guess is correct - display a message 'Correct!', display the number and change css
   } else if (userGuess === secretNumber) {
     message.innerHTML = "Correct!";
-    document.querySelector('.number').innerHTML = secretNumber;
-    document.querySelector('.number').style.background = '#678D58';
-    document.querySelector('.number').style.color = '#FFF8F0';
-    document.querySelector('.number').style.fontWeight = '700';
-    message.style.color = '#678D58';
-    message.style.fontWeight = '700';
+    changeStyleOnCorrect();
 
     // set new highscore if it's greater than the existing one
     if (score > highscore) {
@@ -38,24 +53,10 @@ checkButton.addEventListener('click', (e) => {
       document.querySelector('.highscore').innerHTML = `${score} (new record!)`;
     }
 
-    // if the user's guess is greater than the number, decrease the score by one
   } else if (userGuess > secretNumber) {
-    score--;
-    scoreSpan.innerHTML = score;
-    if (score > 0) {
-      message.innerHTML = "Too high!";
-    } else {
-      message.innerHTML = "Game over!"
-    }
-    // if the user's guess is lower than the number, decrease the score by one
+    updateScore('Too high!');
   } else if (userGuess < secretNumber) {
-    score--;
-    scoreSpan.innerHTML = score;
-    if (score > 0) {
-      message.innerHTML = "Too low!";
-    } else {
-      message.innerHTML = "Game over!"
-    }
+    updateScore('Too low!');
   }
 })
 
