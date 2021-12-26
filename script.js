116a1c72ed1e4f53f06e570a0ddb6cfc16c7e1d9
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-26 09:51:15 +0100

    Store highscore using localStorage

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 
 // Define the secret number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(`The secret number is ${secretNumber}`)
+
+// Check local storage for the highscore. If none - set to zero
+let highscore = localStorage.getItem('highscore') ? localStorage.getItem('highscore') : 0;
+document.querySelector('.highscore').innerHTML = highscore;
 
 // Add an event listener to the "Check!" button
 const checkButton = document.querySelector('.check');
@@ -13,15 +16,30 @@ const scoreSpan = document.querySelector('.score');
 
 let score = 20;
 
+// Check user's input
 checkButton.addEventListener('click', (e) => {
   const userGuess = Number(input.value);
+  // if the user provided no number - display a message 'No number!'
   if (!userGuess) {
     message.innerHTML = "No number!"
+    // if the user's guess is correct - display a message 'Correct!', display the number and change css
   } else if (userGuess === secretNumber) {
     message.innerHTML = "Correct!";
     document.querySelector('.number').innerHTML = secretNumber;
     document.querySelector('.number').style.background = '#678D58';
-  } else if (userGuess > secretNumber) { // decrease the score by one in the case of a wrong guess
+    document.querySelector('.number').style.color = '#FFF8F0';
+    document.querySelector('.number').style.fontWeight = '700';
+    message.style.color = '#678D58';
+    message.style.fontWeight = '700';
+
+    // set new highscore if it's greater than the existing one
+    if (score > highscore) {
+      localStorage.setItem('highscore', score);
+      document.querySelector('.highscore').innerHTML = `${score} (new record!)`;
+    }
+
+    // if the user's guess is greater than the number, decrease the score by one
+  } else if (userGuess > secretNumber) {
     score--;
     scoreSpan.innerHTML = score;
     if (score > 0) {
@@ -29,6 +47,7 @@ checkButton.addEventListener('click', (e) => {
     } else {
       message.innerHTML = "Game over!"
     }
+    // if the user's guess is lower than the number, decrease the score by one
   } else if (userGuess < secretNumber) {
     score--;
     scoreSpan.innerHTML = score;
@@ -40,6 +59,7 @@ checkButton.addEventListener('click', (e) => {
   }
 })
 
+// Reload the page if the user clicks on the 'Again!' button
 againButton.addEventListener('click', (e) => {
   window.location.reload();
 })
